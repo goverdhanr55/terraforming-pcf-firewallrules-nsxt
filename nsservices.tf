6274f42bfd37cb7a44d29e86f9229765f5a9de58
Goverdhan Kanapuram <goverdhan.r55@gmail.com>
2019-05-06 16:44:02 -0400

    added nsservices.tf

---
+++ nsservices.tf
@@ -0,0 +1,29 @@
+################################################################
+# NSSERVICES for PCF
+################################################################
+
+# Create NSSERVICES for PCF
+
+# SSH Service for PCF 
+resource "nsxt_l4_port_set_ns_service" "pcf_ssh_ns_service" {
+  description       = "SSH Service for PCF"
+  display_name      = "pcf_ssh_ns_service"
+  protocol          = "TCP"
+  destination_ports = ["22"]
+}
+
+# HTTP Service for PCF
+resource "nsxt_l4_port_set_ns_service" "pcf_http_ns_service" {
+  description       = "HTTP Service for PCF"
+  display_name      = "pcf_http_ns_service"
+  protocol          = "TCP"
+  destination_ports = ["80"]
+}
+
+# HTTPS Service for PCF
+resource "nsxt_l4_port_set_ns_service" "pcf_https_ns_service" {
+  description       = "HTTPS Service for PCF"
+  display_name      = "pcf_https_ns_service"
+  protocol          = "TCP"
+  destination_ports = ["443"]
+}
