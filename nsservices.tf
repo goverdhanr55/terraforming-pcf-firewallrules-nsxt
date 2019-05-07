f9c2b19574d0f7eba41a89a8ecda1daa5d62cb5c
Goverdhan Kanapuram <goverdhan.r55@gmail.com>
2019-05-07 15:25:38 -0400

    added services

--- nsservices.tf
+++ nsservices.tf
@@ -28,9 +28,40 @@ resource "nsxt_l4_port_set_ns_service" "pcf_https_ns_service" {
   destination_ports = ["443"]
 }
 
+# DNS Service for PCF - TCP
+resource "nsxt_l4_port_set_ns_service" "pcf_dns_tcp_ns_service" {
+  description       = "DNS Service for PCF - TCP"
+  display_name      = "pcf_dns_tcp_ns_service"
+  protocol          = "TCP"
+  destination_ports = ["53"]
+}
+
+# DNS Service for PCF - UDP
+resource "nsxt_l4_port_set_ns_service" "pcf_dns_udp_ns_service" {
+  description       = "DNS Service for PCF - UDP"
+  display_name      = "pcf_dns_udp_ns_service"
+  protocol          = "UDP"
+  destination_ports = ["53"]
+}
+
 # IPSec Service for PCF
 resource "nsxt_ip_protocol_ns_service" "pcf_ipsec_ns_service" {
   description  = "IPSec Service for PCF"
   display_name = "pcf_ipsec_ns_service"
   protocol     = "AH"
+}
+
+# IPSec Encapsulation Service for PCF
+resource "nsxt_ip_protocol_ns_service" "pcf_ipsec_encapsulation_ns_service" {
+  description  = "IPSec Encapsulation Service for PCF"
+  display_name = "pcf_ipsec_encapsulation_ns_service"
+  protocol     = "ESP"
+}
+
+# IPSec UDP Encapsulation Service for PCF
+resource "nsxt_l4_port_set_ns_service" "pcf_ipsec_udp_encapsulation_ns_service" {
+  description       = "IPSec UDP Encapsulation Service for PCF"
+  display_name      = "pcf_ipsec_udp_encapsulation_ns_service"
+  protocol          = "UDP"
+  destination_ports = ["500"]
 }
